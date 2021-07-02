1b2455c4b2510d1e1ee1a24538a317dad30b3767
TimzOwen <ktimothy@kabarak.ac.ke>
2021-07-02 10:59:15 +0300

    updated database Room files

---
+++ app/src/main/java/com/codewithtimzowen/androidroomdb/database/UserDao.java
@@ -0,0 +1,22 @@
+package com.codewithtimzowen.androidroomdb.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Query("SELECT * FROM  User")
+    List<User> getAllUsers();
+
+    @Insert
+    void insertUser(User... users);
+
+    @Delete()
+    void delete( User user);
+
+}
