82077ec7e0ce86fd3230e4fd35e356c18e229a3f
TimzOwen <ktimothy@kabarak.ac.ke>
2021-07-02 12:51:52 +0300

    finished setting up get from db

--- app/src/main/java/com/codewithtimzowen/androidroomdb/AddNewUserActivity.java
+++ app/src/main/java/com/codewithtimzowen/androidroomdb/AddNewUserActivity.java
@@ -3,6 +3,12 @@ package com.codewithtimzowen.androidroomdb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.codewithtimzowen.androidroomdb.database.AppDatabase;
+import com.codewithtimzowen.androidroomdb.database.User;
 
 public class AddNewUserActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class AddNewUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_user);
+
+        final EditText first_name = findViewById(R.id.etFName);
+        final EditText last_name = findViewById(R.id.etLName);
+        Button btnSave = findViewById(R.id.btnSaveUser);
+
+        btnSave.setOnClickListener(v -> {
+
+            saveNewUser(first_name.getText().toString(), last_name.getText().toString());
+
+        });
+    }
+
+    //save data to the database
+    public void saveNewUser(String first_name, String last_name){
+
+        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
+
+        User user = new User();
+        user.firstName = first_name;
+        user.lastName = last_name;
+
+        db.userDao().insertUser();
+
+        finish();
+
     }
 }
